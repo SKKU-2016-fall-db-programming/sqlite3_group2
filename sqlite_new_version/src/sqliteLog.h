c7f940efb8832aca47f16a55414ad1f39645bf9d
JiaYoon <yoon4591@gmail.com>
2016-12-23 09:50:35 +0900

    add new version

---
+++ sqlite_new_version/src/sqliteLog.h
@@ -0,0 +1,7 @@
+#ifndef SQLITE_LOGGING                                                           
+#define SQLITE_LOGGING                                                           
+int log_fd;                                                                  
+void *log_buffer;                                                            
+int is_open;                                                                 
+unsigned int lastLsn;                                                        
+#endif     
